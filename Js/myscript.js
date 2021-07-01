d10e95806c1be0ac55cf775a1a9a279e4da74417
rays63 <raymondmhz63@gmail.com>
2021-07-01 18:48:01 +0545

    Update myscript.js

--- Js/myscript.js
+++ Js/myscript.js
@@ -34,6 +34,21 @@ scrollBtn.addEventListener("click", () => {
   document.documentElement.scrollTop = 0;
 });
 
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 10000); // Change image every 2 seconds
+}
+
 var slideIndex = 1;
 showSlides(slideIndex);
 

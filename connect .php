ed913d0ec3265c0f74153d6cedbf0611172b21b5
rays63 <raymondmhz63@gmail.com>
2021-07-14 08:29:20 +0545

    php

---
+++ connect .php
@@ -0,0 +1,20 @@
+<?php 
+	$name = $_POST['name']
+	$email = $_POST['email']
+	$message = $_POST['message']
+
+	//database connect
+	$conn = new mysqli('sql6.freesqldatabase.com','sql6425026','Sq8nKFFbMJ','sql6425026');
+	if ($conn->connect_error) {
+		// code...
+		die('Connection Failed : '.conn->connect_error);
+	}
+	else{
+		$stmt = $conn->prepare("insert into contactme(name, email, message) values(?, ?, ?)");
+		$stmt->bind_param("ssssi",$name,$email,$message);
+		$stmt->execute();
+		echo "send Succesfully.....";
+		$stmt->close();
+		$conn->close();
+	}
+?>
